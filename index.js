c6b05701ad81934bbb81f8338c23c3f76df5aacb
mehmet baran <mehmetbaran@mehmetbaran.net>
2018-07-08 00:28:30 +0300

    fix #4: trx(translate) command added

--- index.js
+++ index.js
@@ -3,18 +3,24 @@
 const program = require('commander');
 const packageJson = require("./package.json");
 const go = require("./src/commands/go");
-const search = require("./src/commands/search");
+const src = require("./src/commands/src");
+const trx = require("./src/commands/trx");
  
 program.version(packageJson.version);
 
 program.command("go <address>").action(function(address) { go(address) })
 
 program
-  .command("search <term>")
+  .command("src <term>")
   .option('-b, --browser', false)
   .option('-c, --count <n>', "result count", 5, parseInt)
   .option('-s, --skip <n>', "result count", 0, parseInt)
-  .action(function(term, options) { search(term, options) })
+  .action(function(term, options) { src(term, options) })
+
+program
+  .command("trx <term>")
+  .option("-l --language <s>", "translation languages", "en-tr")  
+  .action(function(term, options) { trx(term, options) })
 
 
 
---
+++ src/commands/trx/index.js
@@ -0,0 +1,14 @@
+const translate = require('google-translate-api');
+
+module.exports = function (term, options) {
+  var langs = options.language.split('-');
+
+  translate(term, {from: langs[0], to: langs[1]}).then(res => {
+    console.log(res.text);
+    if(res.from.text.autoCorrected) {
+      console.log(res.from.text.value);      
+    }
+  }).catch(err => {
+    console.error(err);
+  });
+}
