f5138970165c077c46382899ee3248be30131c3d
mehmet baran <mehmetbaran@mehmetbaran.net>
2018-07-08 15:48:26 +0300

    eslint configured

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const packageJson = require("./package.json");
-const go = require("./src/commands/go");
-const src = require("./src/commands/src");
-const trx = require("./src/commands/trx");
- 
+const packageJson = require('./package.json');
+const go = require('./src/commands/go');
+const src = require('./src/commands/src');
+const trx = require('./src/commands/trx');
+
 program.version(packageJson.version);
 
-program.command("go <address>").action(function(address) { go(address) })
+program.command('go <address>').action(function(address) { go(address) });
 
 program
-  .command("src <term>")
+  .command('src <term>')
   .option('-b, --browser', false)
-  .option('-c, --count <n>', "result count", 5, parseInt)
-  .option('-s, --skip <n>', "result count", 0, parseInt)
-  .action(function(term, options) { src(term, options) })
+  .option('-c, --count <n>', 'result count', 5, parseInt)
+  .option('-s, --skip <n>', 'result count', 0, parseInt)
+  .action(function(term, options) { src(term, options) });
 
 program
-  .command("trx <term>")
-  .option("-l --language <s>", "translation languages", "en-tr")  
-  .action(function(term, options) { trx(term, options) })
-
-
+  .command('trx <term>')
+  .option('-l --language <s>', 'translation languages', 'en-tr')
+  .action(function(term, options) { trx(term, options) });
 
 program.parse(process.argv);
